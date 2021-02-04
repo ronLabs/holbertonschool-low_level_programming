77262c741d3923ea1732a6df4df4d49df06241f7
ronLabs <ron.altamir@outlook.com>
2021-02-04 22:29:02 +0000

    Creating a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,10 @@
 #include <time.h>
 
 /**
- * main - a random number is assigned to the variable n each time it is executed
+ * main - random number is assigned to the variable n each time it is executed
  * if the number is greater than 0: is positive, if the number is 0: is zero
  * if the number is less than 0: is negative, followed by a new line
+ * Return: 0
  */
 int main(void)
 {
@@ -13,18 +14,17 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
-	if ( n>0 )
+	if (n > 0)
 	{
 		printf("%d is positive\n", n);
-    }
-	else if ( n==0 )
+	}
+	else if (n < 0)
 	{
-		printf("%d is zero\n", n);
+		printf("%d is negative\n", n);
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is zero\n", n);
 	}
 	return (0);
 }

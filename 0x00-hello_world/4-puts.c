ccf972a2d6b0348ca7597bb8de2bf3a44214daaa
ronLabs <ron.altamir@outlook.com>
2021-02-04 03:27:49 +0000

    Print a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * Description: print - Prints out a string to stdout.
+ */
+int print(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+}

5a6475a7a46adfa27837a94a8d946b07d936bb9b
ronLabs <ron.altamir@outlook.com>
2021-02-09 20:13:47 +0000

    Write a string with _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * main - program can prints Holberton
+ * Return: int
+ */
+
+int main(void)
+{
+	int n = 0;
+	char str_hol[9] = "Holberton";
+
+	while (n < 9)
+	{
+		_putchar(str_hol[n]);
+		n +=1;
+	}
+	_putchar('\n');
+	return (0);
+}

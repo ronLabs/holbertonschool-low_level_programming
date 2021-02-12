a742c0cbe01ea710ca04e49426f375c9a0bda56b
ronLabs <ron.altamir@outlook.com>
2021-02-12 00:15:27 +0000

    Function that prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - prints the numbers
+ * 
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int num=48;
+
+	while (num >= 48 && num <= 57)
+	{
+		_putchar(num);
+		num += 1;
+	}
+	_putchar('\n');
+}

fec2e0fde6b487f32236eb9e55462927783eb6ae
ronLabs <ron.altamir@outlook.com>
2021-02-15 03:36:20 +0000

    Print numbers 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 9
+ * Return: nothing
+ */
+int main(void)
+{
+	char num;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}

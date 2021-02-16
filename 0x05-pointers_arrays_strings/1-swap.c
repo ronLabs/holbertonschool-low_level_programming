d9d71099aabd48b6a85c3534ff67c81890090d80
ronLabs <ron.altamir@outlook.com>
2021-02-16 19:43:08 +0000

    Function that swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+ 
+/**
+ * swap_int - takes pointer to 2 integers as parameters and updates
+ * the value a and b points to to 42 and 98 respectively
+ * @a: pointer to an integer
+ * @b: pointer to an integer
+ * Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}

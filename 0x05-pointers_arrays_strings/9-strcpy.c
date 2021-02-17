1f745835da9c3d297e105271b9cdd33cf31787ba
ronLabs <ron.altamir@outlook.com>
2021-02-17 14:35:09 +0000

    Function that copies a string including terminating null

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,19 +3,19 @@
 /**
  * _strcpy - Function that copies the string pointed to by src,
  * to buffer pointed to by dest
- * @dest: pointer to char
- * @src: pointer to char
+ * @dest: pointer to destination char
+ * @src: pointer to source char
  * Return: the pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
+	int i;
 
-	for (i = 0; i < 98; i++)
+	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	*(dest + i) = '\0';
+	dest[i] = '\0';
 	return (dest);
 }

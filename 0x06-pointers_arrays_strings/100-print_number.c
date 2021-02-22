1138967ef3846ee31983084b7efc51dc1c64f0f7
ronLabs <ron.altamir@outlook.com>
2021-02-22 18:25:32 +0000

    Function that prints an integer

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -16,7 +16,11 @@ void print_number(int n)
 	}
 	if (n == 0)
 		_putchar('0');
-	if (n >= 1 && n <= 99)
+	if (n >= 1 && n <= 9)
+	{
+		_putchar(n + '0');
+	}
+	if (n >= 10 && n <= 99)
 	{
 		n1 = n / 10;
 		n2 = n % 10;

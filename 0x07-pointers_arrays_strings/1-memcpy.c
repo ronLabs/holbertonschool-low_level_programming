6f884080cf7f47df48314e343e7d581b7c58dce6
ronLabs <ron.altamir@outlook.com>
2021-02-23 00:22:19 +0000

    Function that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _memcpy - function that fills memory with a constant byte.
+ * @src: source position 
+ * @dest: dest position
+ * @n: size of bytes
+ * Return: pointer to a char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (i < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}

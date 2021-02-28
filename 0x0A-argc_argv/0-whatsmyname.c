6cfbc08c2eac76e43deae722eadf1d36e23d70fc
ronLabs <ron.altamir@outlook.com>
2021-02-28 18:28:47 +0000

    Program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,16 +4,15 @@
  * main - Program that prints its name
  * @argc: argument counter
  * @argv: argument vector pointer to a string
- * 
- * Return: an integer 
+ * Return: an integer
  */
 
-int main( int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{	
+	{
 		if (argc == 1)/*indiques position 1 of the string*/
 			printf("%s\n", argv[i]);
 		else

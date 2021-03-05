db58acad7a38c8dba84db70b75c7e7c4de16417a
ronLabs <ron.altamir@outlook.com>
2021-03-05 04:18:33 +0000

    Function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <stdio.h>
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);/*prototype function that allocates memory using malloc*/
+#endif

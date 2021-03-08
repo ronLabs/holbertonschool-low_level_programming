2f2a693a8a15143d0c19e5172a55f2ec6c47aac8
ronLabs <ron.altamir@outlook.com>
2021-03-08 03:25:57 +0000

    Function that reallocates a memory block using malloc and free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -35,7 +35,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 	rell = (char *)new_array;
 	cpy_ptr = (char *)ptr;
-	for (i = 0; i < new_size; i++)
+	for (i = 0; i < old_size && i < new_size; i++)
 	{
 		rell[i] = cpy_ptr[i];
 	}

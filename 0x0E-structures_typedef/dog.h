df0fd12a103a5863be3c46a5151ff2222df61873
ronLabs <ron.altamir@outlook.com>
2021-03-09 03:29:52 +0000

    Defining a new type dog_t as a new name for the type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
-typedef struct dog dog_t;/*Defining a new type dog_t as a new name for the type struct dog */
+/*Defining a new type dog_t as a new name for the type struct dog */
+typedef struct dog dog_t;
 
 /**
  * struct dog - Dog characteristics

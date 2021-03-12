64589bd9b3fa89e298015fb192858aa61967b28d
ronLabs <ron.altamir@outlook.com>
2021-03-12 20:23:37 +0000

    Writing a 64-bit program in assembly that prints Hello, Holberton

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,16 +1,13 @@
 section .data
-    text db "Hello, Holberton",10
- 
-section .text
-    global main
- 
-main:
-    mov rax, 1
-    mov rdi, 1
-    mov rsi, text
-    mov rdx, 14
-    syscall
 
-    mov rax, 60
-    mov rdi, 0
-    syscall
+	message db "Hello, Holberton", 10
+
+	section .text
+
+	global main
+main:
+	mov rax, 1
+	mov rdi, 1
+	mov rsi, message
+	mov rdx, 17
+	syscall

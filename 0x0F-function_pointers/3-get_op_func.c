8f989a260069235d3f758fd7c186a11783ebac3c
ronLabs <ron.altamir@outlook.com>
2021-03-12 21:20:54 +0000

    Program that performs simple operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,26 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+        	{"+", op_add},
+        	{"-", op_sub},
+        	{"*", op_mul},
+        	{"/", op_div},
+        	{"%", op_mod},
+        	{NULL, NULL}
+	};
+	int i;
+	
+	for (i = 0; i < 5; i++)
+	{
+		if (*ops[i].op == *s && s[1] == 0)
+		{
+			return (ops[i].f);
+		}
+	}
+	printf("Error\n");
+	exit(99);
+}

62d74268368ea67e8ce95451200cb608ccfeadc4
ronLabs <ron.altamir@outlook.com>
2021-03-19 04:14:49 +0000

    function that prints all the elements

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 
 /**
- * struct list - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
@@ -18,5 +18,10 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+int _putchar(char c);
 size_t print_list(const list_t *h);
-#endif /*END OF LISTS_H*/
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+#endif

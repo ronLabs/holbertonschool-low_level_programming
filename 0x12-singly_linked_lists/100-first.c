2f9f70290d5eb3b0a48be5a59845f2a17cf31d0e
ronLabs <ron.altamir@outlook.com>
2021-03-19 04:46:57 +0000

    function that prints before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void tortoise (void) __attribute__((constructor));
+
+/**
+ * tortoise - Prints a string before the main function is executed.
+ * Return: always void.
+ */
+void tortoise(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}

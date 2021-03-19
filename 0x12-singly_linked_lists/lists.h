6959055b5b18af05159d85195e6c32b431de8e88
ronLabs <ron.altamir@outlook.com>
2021-03-19 04:09:43 +0000

    function that prints all the elements

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -12,11 +12,11 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-typedef struct list
+typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct list *next;
+	struct list_s *next;
 } list_t;
 size_t print_list(const list_t *h);
 #endif /*END OF LISTS_H*/

549d0da58199f661fa280f61fada1e0882b12305
ronLabs <ron.altamir@outlook.com>
2021-03-25 04:51:01 +0000

    function that returns the sum of all the data

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - function that returns the sum of all the data (n) 
+ * @head:pointer value.
+ * Return: an integer
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+	while (head)
+	{
+		sum = sum + head->n;
+		head = head->next;
+	}
+	return (sum);
+}

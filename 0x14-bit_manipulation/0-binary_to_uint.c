f30b4d3715a836cfa03c18e85105c38952d3e7da
ronLabs <ron.altamir@outlook.com>
2021-03-26 00:38:12 +0000

    function that converts a binary number to an int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * binary_to_uint - function that converts a binary number to an unsigned int 
+ * binary_to_uint - function that converts a binary number to an unsigned int
  * @b: a pointer to a string
  * Return: the converted number, or 0 if
  * there is one or more chars in the string b that is not 0 or 1

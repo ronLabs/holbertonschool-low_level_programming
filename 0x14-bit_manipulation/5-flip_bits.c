980256a315114662874d5ffc0907362e9c7cbb55
ronLabs <ron.altamir@outlook.com>
2021-03-29 02:35:28 +0000

    function that returns the number of bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * flip_bits - function that returns the number of bits you would need to flip 
+ * to get from one number to another.
+ * @n: value
+ * @m: value
+ * Return: unsigned int
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i = 0;
+	unsigned long int j;
+
+	j = n ^ m;
+	while (j)
+	{
+		i += j & 1;
+		j = j >> 1;
+	}
+	return (i);
+}

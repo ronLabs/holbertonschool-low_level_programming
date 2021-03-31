6e3d49900969248d9a6673bb7dd3277e3648ccc6
ronLabs <ron.altamir@outlook.com>
2021-03-31 03:22:54 +0000

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,15 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	/* open() and creat() return the new file descriptor*/
+	/* or -1 if an error occurred*/
 	if (fd == -1)
 		return (-1);
 
-	while (text_content[i])
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[i])/*the length of the content*/
 		i++;
 
 	wr = write(fd, text_content, i);

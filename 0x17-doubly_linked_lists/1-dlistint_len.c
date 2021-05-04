4f4558764a46d3bdaa8d02392f010a5341cf40f2
ronLabs <ron.altamir@outlook.com>
2021-05-04 03:25:08 +0000

    function that returns the number of elements

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,4 +1,11 @@
 #include "lists.h"
+/**
+* dlistint_len - function
+* @h: pointer to head node of list
+*
+* Description: function to return number of elements in a lnkd list
+* Return: Number of elements
+*/
 
 size_t dlistint_len(const dlistint_t *h)
 {
